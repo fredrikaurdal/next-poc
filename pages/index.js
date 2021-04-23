8b5be93295088092b3cb8ebc4f2df2a50dcbbf95
Fredrik <57868076+fredrikassignments@users.noreply.github.com>
2021-04-23 20:18:44 +0300

    Updated

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.scss';
-
 export default function Home() {
   return (
-    <div className={styles.component}>
-      <h1 className="heading--home">
-        Book Your Stay <span>In Bergen</span>
-      </h1>
-    </div>
+    <>
+      <div className="home--grid">
+        <h1 className="home--heading">
+          Book Your Stay <span>In Bergen</span>
+        </h1>
+        <h1 className="home--heading">
+          Book Your Stay <span>In Bergen</span>
+        </h1>
+      </div>
+    </>
   );
 }
