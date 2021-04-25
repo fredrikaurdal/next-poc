ec657984df0d273e07185e6a56e188e93613f6d7
Fredrik <fredrik.a+noroff@protonmail.ch>
2021-04-25 20:53:31 +0300

    Updated

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,29 @@
-import styles from '../styles/sass/components/Home.module.scss';
+import styles from '../styles/sass/pages/Home.module.scss';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
-    <div className={styles.background}>
-      <div className={styles.grid}>
-        <div className={styles.grid__left}>
-          <div className={styles.content}>
-            <h1 className={styles.heading}>
-              Book Your Stay <span>In Bergen</span>
-            </h1>
-            <div className="description">
-              Comfortable rooms in the heart of the city centre
+    <>
+      <Navbar />
+      <div className={styles.background}>
+        <div className={styles.grid}>
+          <div className={styles.grid__left}>
+            <div className={styles.content}>
+              <h1 className={styles.heading}>
+                Book Your Stay <span>In Bergen</span>
+              </h1>
+              <div className="description">
+                Comfortable rooms in the heart of the city centre
+              </div>
+              <input className={styles.input} placeholder="Enter hotel name" />
             </div>
           </div>
+          <img
+            src="https://project-exam-2.s3.eu-north-1.amazonaws.com/sharon_christina_rorvik_fwcnv0_W_Rs_TY_unsplash_cropped_compressed_5480db2dfd.jpg"
+            alt="Home"
+          />
         </div>
-        <img
-          src="https://project-exam-2.s3.eu-north-1.amazonaws.com/sharon_christina_rorvik_fwcnv0_W_Rs_TY_unsplash_cropped_compressed_5480db2dfd.jpg"
-          alt="Home"
-        />
       </div>
-    </div>
+    </>
   );
 }
