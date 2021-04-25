c46560ae36eb470b7130e4bb7c2fd5741f22cb40
Fredrik <fredrik.a+noroff@protonmail.ch>
2021-04-25 18:29:27 +0300

    Updated

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,18 @@ import styles from '../styles/sass/components/Home.module.scss';
 
 export default function Home() {
   return (
-    <>
-      <div className={styles.home__grid}>
-        <h1 className={styles.heading}>
-          Book Your Stay <span>In Bergen</span>
-        </h1>
-        <img className="home__image" />
+    <div className={styles.background}>
+      <div className={styles.grid}>
+        <div className={styles.grid__left}>
+          <h1 className={styles.heading}>
+            Book Your Stay <span>In Bergen</span>
+          </h1>
+        </div>
+        <img
+          src="https://project-exam-2.s3.eu-north-1.amazonaws.com/sharon_christina_rorvik_fwcnv0_W_Rs_TY_unsplash_cropped_compressed_5480db2dfd.jpg"
+          alt="Home"
+        />
       </div>
-    </>
+    </div>
   );
 }
