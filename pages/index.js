798dfd022d4ed254d3d217e0b38d4583524ebba8
Fredrik <fredrik.a+noroff@protonmail.ch>
2021-04-25 20:08:08 +0300

    Updated

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,14 @@ export default function Home() {
     <div className={styles.background}>
       <div className={styles.grid}>
         <div className={styles.grid__left}>
-          <h1 className={styles.heading}>
-            Book Your Stay <span>In Bergen</span>
-          </h1>
+          <div className={styles.content}>
+            <h1 className={styles.heading}>
+              Book Your Stay <span>In Bergen</span>
+            </h1>
+            <div className="description">
+              Comfortable rooms in the heart of the city centre
+            </div>
+          </div>
         </div>
         <img
           src="https://project-exam-2.s3.eu-north-1.amazonaws.com/sharon_christina_rorvik_fwcnv0_W_Rs_TY_unsplash_cropped_compressed_5480db2dfd.jpg"
