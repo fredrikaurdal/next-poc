c0e9efd1aa824be38fc95aae452aad5036b56596
Fredrik <fredrik.a+noroff@protonmail.ch>
2021-04-25 21:36:38 +0300

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,11 @@ import styles from '../styles/sass/components/Navbar.module.scss';
 export default function Navbar() {
   return (
     <div className={styles.navbar}>
-      <img src="https://project-exam-2.s3.eu-north-1.amazonaws.com/logo_0fa503e974.svg" />
+      <Link href="/">
+        <a>
+          <img src="https://project-exam-2.s3.eu-north-1.amazonaws.com/logo_0fa503e974.svg" />
+        </a>
+      </Link>
       <div className={styles.navbar__links}>
         <Link href="/">
           <a>Home</a>
