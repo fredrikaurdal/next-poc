1d490a55002def31b8871a46798878468dbb6874
Fredrik <57868076+fredrikassignments@users.noreply.github.com>
2021-04-27 18:48:25 +0300

    Updated

--- components/HotelCard.js
+++ components/HotelCard.js
@@ -19,7 +19,7 @@ export default function HotelCard(props) {
           <div className="price">€{props.price}</div>
           <div className={`info ${styles.price_wrapper__info}`}>Per night</div>
         </div>
-        <Button text="More" style="button__hotel_listing" id={props.id} />
+        <Button text="More" style="button--hotel-listing" id={props.id} />
       </div>
     </div>
   );
