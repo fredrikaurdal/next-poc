fd32489eb2da2ccd48d5390839ca641ce1c72c6e
Fredrik <35367024+fredrikaurdal@users.noreply.github.com>
2021-05-01 18:18:30 +0300

    Updated

--- components/Input.js
+++ components/Input.js
@@ -2,11 +2,17 @@ import styles from '../styles/sass/components/Input.module.scss';
 
 export default function Input(props) {
   return (
-    <input
-      type={props.type}
-      className={styles.input}
-      placeholder={props.placeholder}
-      name={props.name}
-    />
+    <>
+      <input
+        type={props.type}
+        className={styles.input}
+        placeholder={props.placeholder}
+        name={props.name}
+        required={props.required}
+        type={props.type}
+        // ref={props.registerRef}
+      />
+      {/* {props.errors.name && <span>{props.errors.name.message}</span>} */}
+    </>
   );
 }
--- components/Modal.js
+++ components/Modal.js
@@ -1,32 +1,39 @@
+import { useState } from 'react';
 import styles from '../styles/sass/components/Modal.module.scss';
 import Input from '../components/Input';
 import Button from '../components/Button';
 import { CLOSE } from '../constants/assets';
+import { BASE_URL } from '../constants/api';
 
 export default function Modal({ open, onClose, name, email, number }) {
   if (!open) return null;
 
+  const [status, setStatus] = useState(null);
+
   const submitEnquiry = async (event) => {
     event.preventDefault();
 
-    const res = await fetch(
-      'https://strapi-project-exam-2.herokuapp.com/enquiries',
-      {
-        body: JSON.stringify({
-          Name: event.target.name.value,
-          Email: event.target.email.value,
-          Phone_number: event.target.phone_number.value,
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        method: 'POST',
-      }
-    );
+    const res = await fetch(BASE_URL + 'enquiries', {
+      body: JSON.stringify({
+        Name: event.target.Name.value,
+        Email: event.target.Email.value,
+        Phone_number: event.target.Phone_number.value,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    });
 
-    const result = await res.json();
+    event.target.reset();
 
-    console.log(result);
+    const result = await res;
+
+    // result = await res.json();
+
+    setStatus(result.status);
+
+    console.log(result.status);
   };
 
   return (
@@ -36,16 +43,33 @@ export default function Modal({ open, onClose, name, email, number }) {
         <img src={CLOSE} className={styles.close_button} onClick={onClose} />
         <form onSubmit={submitEnquiry} className={styles.form}>
           {name === true ? (
-            <Input placeholder={'Name*'} required name="name" />
+            <Input
+              placeholder={'Name *'}
+              required={true}
+              name="Name"
+              // errors={result}
+            />
           ) : null}
           {email === true ? (
-            <Input placeholder={'Email*'} required name="email" />
+            <Input
+              placeholder={'Email *'}
+              required={true}
+              name="Email"
+              type="email"
+            />
           ) : null}
           {number === true ? (
-            <Input placeholder={'Phone number'} name="phone_number" />
+            <Input
+              placeholder={'Phone number'}
+              name="Phone_number"
+              type="number"
+            />
           ) : null}
 
           <Button text="Enquire" style="button__input-submit" input={true} />
+          <div className={styles.status}>
+            {status === 200 && 'Submitted successfully'}
+          </div>
         </form>
       </div>
     </>
