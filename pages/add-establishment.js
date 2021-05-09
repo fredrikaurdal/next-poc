fff80c90c5048962b33b3474d319f74c1ab101cd
Fredrik <57868076+fredrikassignments@users.noreply.github.com>
2021-05-09 08:15:21 +0300

    Updated

--- pages/add-establishment.js
+++ pages/add-establishment.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import styles from '../styles/sass/pages/Enquiries.module.scss';
+import styles from '../styles/sass/pages/AddEstablishment.module.scss';
 import { BASE_URL } from '../constants/api';
 import Navbar from '../components/DashboardNavbar';
 import Layout from '../components/layouts/Dashboard';
@@ -69,46 +69,52 @@ export default function AddEstablishment() {
     <>
       <Navbar />
       <Layout>
-        <div className={styles.layout__wrapper}>
-          <div className={styles.form_wrapper}>
-            <form onSubmit={submitEnquiry} className={styles.form}>
-              <Input
-                placeholder={'Name *'}
-                name="Name"
-                error={validation['Name']}
-              />
-              <Input
-                placeholder={'Description *'}
-                name="Description"
-                error={validation['Description']}
-                textarea={true}
-              />
-              <Input
-                placeholder={'Rating *'}
-                name="Rating"
-                type="number"
-                error={validation['Rating']}
-              />
-              <Input
-                placeholder={'Price *'}
-                name="Price"
-                type="number"
-                error={validation['Price']}
-              />
-              <Input
-                placeholder={'Address *'}
-                name="Address"
-                error={validation['Address']}
-              />
-              <Button
-                value="Send"
-                style={['button__input_submit']}
-                input={true}
-              />
-              <Validation status={status} />
-            </form>
-          </div>
+        {/* <div className={styles.layout__wrapper}> */}
+        <div className={styles.form_wrapper}>
+          <form onSubmit={submitEnquiry} className={styles.form}>
+            <Input
+              placeholder={'Name *'}
+              name="Name"
+              error={validation['Name']}
+            />
+            <Input
+              placeholder={'Description *'}
+              name="Description"
+              error={validation['Description']}
+              textarea={true}
+            />
+            <Input
+              placeholder={'Rating *'}
+              name="Rating"
+              error={validation['Rating']}
+            />
+            <Input
+              placeholder={'Price *'}
+              name="Price"
+              type="number"
+              error={validation['Price']}
+            />
+            <Input
+              placeholder={'Address *'}
+              name="Address"
+              error={validation['Address']}
+            />
+            {/* Only accept a certain dimension */}
+            <Input
+              placeholder={'Image *'}
+              name="Image"
+              error={validation['Image']}
+              type="file"
+            />
+            <Button
+              value="Send"
+              style={['button__input_submit']}
+              input={true}
+            />
+            <Validation status={status} />
+          </form>
         </div>
+        {/* </div> */}
       </Layout>
     </>
   );
