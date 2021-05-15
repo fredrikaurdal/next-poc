72c119e8b6af1bb0562cb14fc291cc6b5a074909
Fredrik <57868076+fredrikassignments@users.noreply.github.com>
2021-05-15 16:43:14 +0300

    Update messages.js

--- pages/messages.js
+++ pages/messages.js
@@ -2,7 +2,6 @@ import Layout from '../components/layouts/DashboardLayout';
 import MessagesCard from '../components/cards/MessagesCard';
 
 export default function Messages() {
-  // console.log(process.env.BASE_URL);
   return (
     <>
       <Layout>
