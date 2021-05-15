d0a6628552c2830d1c4b415b67c5f9bfcf74d719
Fredrik <35367024+fredrikaurdal@users.noreply.github.com>
2021-05-15 20:10:00 +0300

    Updated

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,14 @@
 import Navbar from '../components/navbar/MainNavbar';
 import Form from '../components/inputs/Form';
+import styles from '../styles/sass/pages/Contact.module.scss';
 
 export default function Hotels() {
   return (
     <>
       <Navbar />
-      <Form formType={'contact'} />
+      <div className={styles.wrapper}>
+        <Form formType={'contact'} />
+      </div>
     </>
   );
 }
